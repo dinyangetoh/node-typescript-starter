c4f6f275d0b421029dffb9b2fff64c33602fde67
David Inyangetoh <dinyangetoh@gmail.com>
2022-12-11 11:15:28 +0100

    remove dist

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sayHello = () => {
-    const name = 'David';
-    const age = '15';
-    console.log(`Say Hello ${name} and ${age}`);
-};
-sayHello();
-//# sourceMappingURL=index.js.map
